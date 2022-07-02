dbf141e4475811dab1b1b760245a9b337cb14b08
Hardik Modha <hardikmodha22@gmail.com>
2022-07-02 13:59:11 +0530

    Clean up

---
+++ debugwg/example/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func main() {
+	wg := sync.WaitGroup{}
+	// wg := debugwg.New()
+
+	// Setup periodic print after every second
+	// stopPeriodicDebug := wg.PeriodicDebug(os.Stdout, 1 * time.Second)
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		var i int32 = 0
+
+		for i = 0; i< 10; i++ {
+			time.Sleep(1 * time.Second)
+			wg.Add(1)
+		}
+
+		for i = 0; i< 9; i++ {
+			time.Sleep(1 * time.Second)
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
+
+	// stopPeriodicDebug()
+}
--- example/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/hardikmodha/debugwg_example
-
-go 1.18
